3226b2a15da6ac01f2027ebcd271bf56a6a8b35e
BulatDes <123715505+BulatDes@users.noreply.github.com>
2023-06-21 10:40:39 +0500

    Add files via upload

---
+++ zad4/zad4/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace zad4
+{
+    public partial class MainPage :TabbedPage
+    {
+        public MainPage (string userLogin, string userPassword)
+        {
+            
+            InitializeComponent();
+        }
+    }
+}
---
+++ zad4/zad4/Page1.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace zad4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 :ContentPage
+    {
+        public Page1 ()
+        {
+            InitializeComponent();
+        }
+        private async void InputButton_Clicked (object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(login.Text) || string.IsNullOrEmpty(pass.Text))
+            {
+                DisplayAlert("Внимание", "Заполните логин и пароль", "OK");
+                return;
+            }
+
+            await Navigation.PushModalAsync(new MainPage(login.Text,pass.Text));
+        }
+    }
+}
